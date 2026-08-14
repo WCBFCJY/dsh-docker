60fffb8b5c9480b0d7f07ccf64aedebc3969e97a
WCBFCJY <11535140+WCBFCJY@users.noreply.github.com>
2026-08-14 21:25:13 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,4 @@ mkdir -p /workspace
 dsh web &
 DSH_PID=$!
 
-exec socat "TCP-LISTEN:${DSH_WEB_PORT:-3080},bind=${DSH_WEB_HOST:-0.0.0.0},fork,reuseaddr" TCP:127.0.0.1:3080
+exec socat "TCP-LISTEN:${DSH_WEB_PORT:-3081},bind=${DSH_WEB_HOST:-0.0.0.0},fork,reuseaddr" TCP:127.0.0.1:3080
