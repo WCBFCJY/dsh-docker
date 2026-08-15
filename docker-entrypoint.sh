3fd6aae88725439a6c3a746bb3459141af672f35
WCBFCJY <11535140+WCBFCJY@users.noreply.github.com>
2026-08-15 22:58:06 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,18 +7,27 @@ mkdir -p /workspace
 
 dsh web &
 
-AUTH_CONF=""
 if [ -n "$DSH_AUTH_USER" ] && [ -n "$DSH_AUTH_PASS" ]; then
     printf '%s:%s\n' "$DSH_AUTH_USER" "$(openssl passwd -apr1 "$DSH_AUTH_PASS")" > /etc/nginx/.htpasswd
-    AUTH_CONF='auth_basic "dsh"; auth_basic_user_file /etc/nginx/.htpasswd;'
+    chmod 600 /etc/nginx/.htpasswd
 fi
 
 if [ ! -f /etc/nginx/nginx.conf ]; then
     HOST="${DSH_WEB_HOST:-0.0.0.0}"
     PORT="${DSH_WEB_PORT:-3081}"
     AUTH_CONF=""
+    AUTH_INTERNAL_LOCATIONS=""
     if [ -f /etc/nginx/.htpasswd ]; then
-        AUTH_CONF='auth_basic "dsh"; auth_basic_user_file /etc/nginx/.htpasswd;'
+        AUTH_CONF='auth_basic "dsh"; auth_basic_user_file /etc/nginx/.htpasswd; error_page 401 = @auth_failed;'
+        AUTH_INTERNAL_LOCATIONS='location @auth_failed {
+            set $challenge "";
+            if ($http_authorization = "") {
+                set $challenge "Basic realm=\"dsh\"";
+                return 401;
+            }
+            add_header WWW-Authenticate $challenge always;
+            return 444;
+        }'
     fi
 
     cat > /etc/nginx/nginx.conf <<NGINXEOF
@@ -27,22 +36,29 @@ events { worker_connections 1024; }
 http {
     include /etc/nginx/mime.types;
     access_log off;
+    map \$http_upgrade \$connection_upgrade {
+        default upgrade;
+        ''      close;
+    }
     server {
         listen ${HOST}:${PORT};
         server_name _;
         location / {
-            $AUTH_CONF
+            ${AUTH_CONF}
             proxy_pass http://127.0.0.1:3080;
             proxy_set_header Host 127.0.0.1:3081;
             proxy_set_header Origin "";
             proxy_http_version 1.1;
             proxy_set_header Upgrade \$http_upgrade;
-            proxy_set_header Connection "upgrade";
+            proxy_set_header Connection \$connection_upgrade;
         }
+        ${AUTH_INTERNAL_LOCATIONS}
     }
 }
 NGINXEOF
 fi
+
+nginx -t
 
 i=0
 while ! curl -sS -o /dev/null http://127.0.0.1:3080/; do
